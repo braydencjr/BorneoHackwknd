1bbd6d46fa906add154835ac846da1083440e0ba
CHIN YIU ERN <yiuernnn@gmail.com>
2026-03-08 01:22:39 +0800

    update gitignore

--- frontend/services/useGoogleAuth.ts
+++ frontend/services/useGoogleAuth.ts
@@ -5,7 +5,8 @@ WebBrowser.maybeCompleteAuthSession();
 
 export function useGoogleAuth() {
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
+    clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID!,
+    scopes: ["openid", "profile", "email"],
   });
 
   return { request, response, promptAsync };
